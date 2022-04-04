0affc562dbc97634f090940e2f8d224e67d82cf9
PatgioK <patgiok@gmail.com>
2022-04-04 13:45:03 -0700

    added posts and form, + associate with account and community and form redirects + create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,16 +11,19 @@ class PostsController < ApplicationController
     end
 
     def new
+        @community = Community.find(params[:community_id])
         @post = Post.new
     end
 
     def create
         @post = Post.new post_values
         @post.account_id = current_account.id
+        @post.community_id = params[:community_id]
 
         if @post.save
-            redirect_to communities_path
+            redirect_to communities_path(@post.community_id)
         else
+            @community = Community.find(params[:community_id])
             render :new
         end
     end
@@ -35,6 +38,6 @@ class PostsController < ApplicationController
     end
 
     def post_values
-        params.require(:post).permit(:name, :url, :rules)
+        params.require(:post).permit(:title, :body)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
 
 
   #resources will create all default routes
-  resources :communities
+  resources :communities do
+    resources :posts
+  end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "public#index"
+  root to: "public#index"
 end
