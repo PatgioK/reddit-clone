8445626234cee5d0202311184d312a8936b6d872
PatgioK <patgiok@gmail.com>
2022-04-04 17:36:06 -0700

    added subscription and models. tut p2, 24:30

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_04_194721) do
+ActiveRecord::Schema.define(version: 2022_04_05_002057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,15 @@ ActiveRecord::Schema.define(version: 2022_04_04_194721) do
     t.datetime "updated_at", null: false
     t.index ["account_id"], name: "index_posts_on_account_id"
     t.index ["community_id"], name: "index_posts_on_community_id"
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.bigint "account_id"
+    t.bigint "community_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["account_id"], name: "index_subscriptions_on_account_id"
+    t.index ["community_id"], name: "index_subscriptions_on_community_id"
   end
 
 end
