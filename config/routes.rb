f64ef250431f8bd920671daf35879a5ca837feb5
PatgioK <patgiok@gmail.com>
2022-04-05 00:38:22 -0700

    added comments, show comments on post, dynamic comment add to post

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   end
   
   resources :subscriptions
+  resources :comments, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "public#index"
