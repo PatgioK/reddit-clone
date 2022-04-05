9741c704c3f4125c42f690b3e2a4597a4f2abf01
PatgioK <patgiok@gmail.com>
2022-04-04 21:51:02 -0700

    added subscription to communities, join / ccreatepost logic

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     resources :posts
   end
   
+  resources :subscriptions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "public#index"
